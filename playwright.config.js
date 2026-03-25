50d11a0543f20922105ab085ed4862297ccfdf00
Fran Teles <dcollege.77262@gmail.com>
2026-03-25 15:04:00 +0000

    Fix web server command for CI

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require('@playwright/test');
 
+const webServerCommand =
+  process.platform === 'win32'
+    ? 'npx serve . -l 3000'
+    : 'python3 -m http.server 3000';
+
 module.exports = defineConfig({
   testDir: './tests',
   use: {
@@ -7,7 +12,7 @@ module.exports = defineConfig({
     headless: true
   },
   webServer: {
-    command: 'npm run start',
+    command: webServerCommand,
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: true
   }
